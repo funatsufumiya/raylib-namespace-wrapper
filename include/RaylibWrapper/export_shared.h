9c590d1a8b469015c4159b70e4b5cb57dfd0a23e
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-03-29 21:43:12 +0900

    fix header for mac

--- include/RaylibWrapper/export_shared.h
+++ include/RaylibWrapper/export_shared.h
@@ -1,38 +1,34 @@
-/* Generated export header for shared library builds */
-#pragma once
 
 #ifndef RAYLIBWRAPPER_SHARED_EXPORT_H
 #define RAYLIBWRAPPER_SHARED_EXPORT_H
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-/* CMake's generate_export_header defines a symbol like <target>_EXPORTS
-	(e.g. raylibwrapper_shared_EXPORTS). Accept common variants so the
-	manual header works whether the target is named `raylibwrapper` or
-	`raylibwrapper_shared`. */
-#  if defined(raylibwrapper_shared_EXPORTS) || defined(raylibwrapper_EXPORTS) || defined(raylibwrapper_EXPORT)
-/* building the DLL */
-#    define RAYLIBWRAPPER_SHARED_EXPORT __declspec(dllexport)
-#  else
-/* using the DLL */
-#    define RAYLIBWRAPPER_SHARED_EXPORT __declspec(dllimport)
-#  endif
+#ifdef RAYLIBWRAPPER_SHARED_STATIC_DEFINE
+#  define RAYLIBWRAPPER_SHARED_EXPORT
 #  define RAYLIBWRAPPER_SHARED_NO_EXPORT
 #else
-#  if __GNUC__ >= 4
-#    define RAYLIBWRAPPER_SHARED_EXPORT __attribute__((visibility("default")))
-#    define RAYLIBWRAPPER_SHARED_NO_EXPORT __attribute__((visibility("hidden")))
-#  else
-#    define RAYLIBWRAPPER_SHARED_EXPORT
-#    define RAYLIBWRAPPER_SHARED_NO_EXPORT
+
+#  ifndef RAYLIBWRAPPER_SHARED_EXPORT
+#    if defined(_WIN32) || defined(_WIN64)
+#      ifdef raylibwrapper_shared_EXPORTS
+         /* We are building this library */
+#        define RAYLIBWRAPPER_SHARED_EXPORT __declspec(dllexport)
+#      else
+         /* We are using this library */
+#        define RAYLIBWRAPPER_SHARED_EXPORT __declspec(dllimport)
+#      endif
+#    else
+         /* Non-Windows platforms */
+#      define RAYLIBWRAPPER_SHARED_EXPORT
+#    endif
+#  endif
+
+#  ifndef RAYLIBWRAPPER_SHARED_NO_EXPORT
+#    define RAYLIBWRAPPER_SHARED_NO_EXPORT 
 #  endif
 #endif
 
 #ifndef RAYLIBWRAPPER_SHARED_DEPRECATED
-#  if defined(_MSC_VER)
-#    define RAYLIBWRAPPER_SHARED_DEPRECATED __declspec(deprecated)
-#  else
-#    define RAYLIBWRAPPER_SHARED_DEPRECATED __attribute__((deprecated))
-#  endif
+#  define RAYLIBWRAPPER_SHARED_DEPRECATED __declspec(deprecated)
 #endif
 
 #ifndef RAYLIBWRAPPER_SHARED_DEPRECATED_EXPORT
@@ -43,6 +39,7 @@
 #  define RAYLIBWRAPPER_SHARED_DEPRECATED_NO_EXPORT RAYLIBWRAPPER_SHARED_NO_EXPORT RAYLIBWRAPPER_SHARED_DEPRECATED
 #endif
 
+/* NOLINTNEXTLINE(readability-avoid-unconditional-preprocessor-if) */
 #if 0 /* DEFINE_NO_DEPRECATED */
 #  ifndef RAYLIBWRAPPER_SHARED_NO_DEPRECATED
 #    define RAYLIBWRAPPER_SHARED_NO_DEPRECATED
